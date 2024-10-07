5284d6592677faf3b02b70ab9d403c4a9f1f6623
donggeoniiii <pkgun0727@gmail.com>
2024-10-07 14:22:22 +0900

    Fix: MemoryMemberRepository가 공통의 저장소를 사용하도록 수정

--- 김영한의 스프링 시리즈/spring-basic/src/main/java/hello/core/member/MemoryMemberRepository.java
+++ 김영한의 스프링 시리즈/spring-basic/src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 public class MemoryMemberRepository implements MemberRepository {
 	// 실무에선 동시성 이슈를 해결하기 위해 ConcurrentHashMap 사용
-	private final Map<Long, Member> store = new HashMap<>();
+	private static final Map<Long, Member> store = new HashMap<>();
 
 	@Override
 	public void save(Member member) {
