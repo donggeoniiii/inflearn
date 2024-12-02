3cb27ae8a2474f1d4c6f9e13fce514ba26cbd78d
donggeoniiii <pkgun0727@gmail.com>
2024-12-02 14:22:51 +0900

    update: 상품 도메인 개발 실습 내용 추가

--- kim-jpa/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ kim-jpa/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -12,13 +12,13 @@ import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
 import jakarta.persistence.ManyToMany;
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
-import lombok.Setter;
 
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "dtype") // 기본값이 DTYPE
-@Getter @Setter
+@Getter
 public abstract class Item {
 
 	@Id @GeneratedValue
@@ -33,4 +33,26 @@ public abstract class Item {
 
 	@ManyToMany(mappedBy = "items")
 	private List<Category> categories = new ArrayList<>();
+
+	// == 비즈니스 로직 == //
+
+	/**
+	 * 수량 증가
+	 * @param quantity 증가량
+	 */
+	public void addStock(int quantity) {
+		this.stock += quantity;
+	}
+
+	/**
+	 * 수량 감소
+	 * @param quantity 감소량
+	 */
+	public void removeStock(int quantity) {
+		int restStock = this.stock - quantity;
+		if (restStock < 0) {
+			throw new NotEnoughStockException("재고 없음");
+		}
+		this.stock -= quantity;
+	}
 }
